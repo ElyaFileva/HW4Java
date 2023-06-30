c88bb9b92bfef5ebb47186dd0ee562010e422717
ElyaFileva <124371423+ElyaFileva@users.noreply.github.com>
2023-06-30 23:22:57 +0300

    main2.java

--- src/main/java/task2/main2.java
+++ src/main/java/task2/main2.java
@@ -1,37 +1,31 @@
 package task2;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashSet;
-import java.util.LinkedList;
-
 
 public class main2 {
     public static void main(String[] args) {
-        String s1 = "()";
-        Deque<Character> deque = new LinkedList<>();
-        System.out.println("isValid(s1, deque) = " + isValid(s1, deque));
+        String s = "({()})";
+        System.out.println("isValid(s) = " + isValid(s));
     }
 
-    public static boolean isValid(String s1, Deque deque) {
-        HashSet<Character> chr = new HashSet<>();
-        chr.add('(');
-        chr.add('{');
-        chr.add('[');
-        System.out.println("chr = " + chr);
-        for (char ch : s1.toCharArray()) {
-            if (chr.contains(ch)) {
-                deque.addFirst(ch);
-            } else {
-                if (!deque.isEmpty()
-                        && ((deque.peekFirst() == '(' && ch == ')')
-                        || ((deque.peekFirst() == '{') && ch == '}')
-                        || ((deque.peekFirst() == '[') && ch == ']'))) {
-                    deque.removeFirst();
-                } else {
+    private static boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+
+        Deque<Character> deque = new ArrayDeque<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '[' || ch == '(' || ch == '{')
+                deque.add(ch);
+            else {
+                if (deque.size() == 0)
                     return false;
-                }
+                if (ch == ')' && deque.peekLast() == '(') deque.pollLast();
+                else if (ch == '}' && deque.peekLast() == '{') deque.pollLast();
+                else if (ch == ']' && deque.peekLast() == '[') deque.pollLast();
             }
         }
-        return deque.isEmpty();
+        return (deque.size() == 0);
     }
 }
